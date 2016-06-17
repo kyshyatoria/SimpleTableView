0e8428747bb666c225bc630b5b36ff6c2badacad
Kanchan Yadav <kanchan.yadav@snapdeal.com>
2016-06-17 17:42:09 +0530

    Initial Commit

---
+++ FruitMania/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FruitMania
+//
+//  Created by Kanchan Yadav on 17/06/16.
+//  Copyright © 2016 Kanchan Yadav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FruitMania/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FruitMania
+//
+//  Created by Kanchan Yadav on 17/06/16.
+//  Copyright © 2016 Kanchan Yadav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
